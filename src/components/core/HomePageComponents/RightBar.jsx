0bc66f4c697d24c8d4245ab542a33d98e117fd93
Ankit Jain <ankit@bosswebtech.com>
2024-02-01 12:39:21 +0530

    Added some static data for demo purpose

--- src/components/core/HomePageComponents/RightBar.jsx
+++ src/components/core/HomePageComponents/RightBar.jsx
@@ -8,6 +8,7 @@ import "swiper/css";
 import "swiper/css/free-mode";
 import FloodAlertCard from "./FloodAlertCard";
 import { FloodAlertData } from "@/data/FloodAlertData";
+import { Height } from "@mui/icons-material";
 
 export default function RightBar({ rightBarStatus }) {
   const [floodAlertData, setFloodAlertData] = useState([]);
@@ -16,35 +17,365 @@ export default function RightBar({ rightBarStatus }) {
       //const response = await fetch("your-api-endpoint");
       const data =  [
         {
-            title : "Damodar",
-            subTitle : "Konar Dam",
+            title : "Agra",
+            subTitle : "Yamuna",
             dataPoints : [
                 {
-                    data : "12/06",
-                    symbol : "red-circle"
+                    data : "Today 6:00",
+                    symbol : "green-circle"
                 },
                 {
-                    data : "14/06",
-                    symbol : "red-circle"
+                    data : "Today 12:00",
+                    symbol : "green-circle"
                 },
                 {
-                    data : "16/06",
-                    symbol : "red-circle"
+                    data : "Today 18:00",
+                    symbol : "green-circle"
                 },
                 {
-                    data : "12/02",
-                    symbol : "red-circle"
+                    data : "Today 00:00",
+                    symbol : "green-circle"
                 },
                 {
-                    data : "15/04",
-                    symbol : "red-circle"
+                    data : "Tom 6:00",
+                    symbol : "green-circle"
                 },
                 {
-                    data : "17/06",
-                    symbol : "red-circle"
+                    data : "Tom 12:00",
+                    symbol : "green-circle"
                 }
             ]
         },
+        {
+          title : "Auraiya",
+          subTitle : "Yamuna",
+          dataPoints : [
+              {
+                  data : "Today 6:00",
+                  symbol : "green-circle"
+              },
+              {
+                  data : "Today 12:00",
+                  symbol : "green-circle"
+              },
+              {
+                  data : "Today 18:00",
+                  symbol : "green-circle"
+              },
+              {
+                  data : "Today 00:00",
+                  symbol : "green-circle"
+              },
+              {
+                  data : "Tom 6:00",
+                  symbol : "green-circle"
+              },
+              {
+                  data : "Tom 12:00",
+                  symbol : "green-circle"
+              }
+          ]
+      },
+      {
+        title : "Chillaghat",
+        subTitle : "Yamuna",
+        dataPoints : [
+            {
+                data : "Today 6:00",
+                symbol : "green-circle"
+            },
+            {
+                data : "Today 12:00",
+                symbol : "green-circle"
+            },
+            {
+                data : "Today 18:00",
+                symbol : "green-circle"
+            },
+            {
+                data : "Today 00:00",
+                symbol : "green-circle"
+            },
+            {
+                data : "Tom 6:00",
+                symbol : "green-circle"
+            },
+            {
+                data : "Tom 12:00",
+                symbol : "green-circle"
+            }
+        ]
+    },
+    {
+      title : "D.R.B",
+      subTitle : "Yamuna",
+      dataPoints : [
+          {
+              data : "Today 6:00",
+              symbol : "green-circle"
+          },
+          {
+              data : "Today 12:00",
+              symbol : "green-circle"
+          },
+          {
+              data : "Today 18:00",
+              symbol : "green-circle"
+          },
+          {
+              data : "Today 00:00",
+              symbol : "green-circle"
+          },
+          {
+              data : "Tom 6:00",
+              symbol : "green-circle"
+          },
+          {
+              data : "Tom 12:00",
+              symbol : "green-circle"
+          }
+      ]
+  },
+  {
+    title : "Etawah",
+    subTitle : "Yamuna",
+    dataPoints : [
+        {
+            data : "Today 6:00",
+            symbol : "green-circle"
+        },
+        {
+            data : "Today 12:00",
+            symbol : "green-circle"
+        },
+        {
+            data : "Today 18:00",
+            symbol : "green-circle"
+        },
+        {
+            data : "Today 00:00",
+            symbol : "green-circle"
+        },
+        {
+            data : "Tom 6:00",
+            symbol : "green-circle"
+        },
+        {
+            data : "Tom 12:00",
+            symbol : "green-circle"
+        }
+    ]
+},
+{
+  title : "Hamirpur",
+  subTitle : "Yamuna",
+  dataPoints : [
+      {
+          data : "Today 6:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Today 12:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Today 18:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Today 00:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Tom 6:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Tom 12:00",
+          symbol : "green-circle"
+      }
+  ]
+},
+{
+  title : "Kalpi",
+  subTitle : "Yamuna",
+  dataPoints : [
+      {
+          data : "Today 6:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Today 12:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Today 18:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Today 00:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Tom 6:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Tom 12:00",
+          symbol : "green-circle"
+      }
+  ]
+},
+{
+  title : "Karnal",
+  subTitle : "Yamuna",
+  dataPoints : [
+      {
+          data : "Today 6:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Today 12:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Today 18:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Today 00:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Tom 6:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Tom 12:00",
+          symbol : "green-circle"
+      }
+  ]
+},
+{
+  title : "Mathura",
+  subTitle : "Yamuna",
+  dataPoints : [
+      {
+          data : "Today 6:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Today 12:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Today 18:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Today 00:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Tom 6:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Tom 12:00",
+          symbol : "green-circle"
+      }
+  ]
+},
+{
+  title : "Mawi",
+  subTitle : "Yamuna",
+  dataPoints : [
+      {
+          data : "Today 6:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Today 12:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Today 18:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Today 00:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Tom 6:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Tom 12:00",
+          symbol : "green-circle"
+      }
+  ]
+},
+{
+  title : "Naini",
+  subTitle : "Yamuna",
+  dataPoints : [
+      {
+          data : "Today 6:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Today 12:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Today 18:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Today 00:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Tom 6:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Tom 12:00",
+          symbol : "green-circle"
+      }
+  ]
+},
+{
+  title : "Paonta Sahib",
+  subTitle : "Yamuna",
+  dataPoints : [
+      {
+          data : "Today 6:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Today 12:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Today 18:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Today 00:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Tom 6:00",
+          symbol : "green-circle"
+      },
+      {
+          data : "Tom 12:00",
+          symbol : "green-circle"
+      }
+  ]
+}
       ]
       //await response.json();
       return data;
@@ -62,7 +393,7 @@ export default function RightBar({ rightBarStatus }) {
     fetchData();
   }, []);
   return (
-    <div className={`${rightBarStatus ? "w-[350px]" : "hidden w-0"} transition-all duration-200 bg-white-100/50 h-[calc(100vh-134px)] z-10 shadow-[-5px_10px_5px] shadow-[#12121210]`}>
+    <div className={`${rightBarStatus ? "w-[350px]" : "hidden w-0"} transition-all duration-200 bg-white-100/50 h-[calc(120vh-134px)] z-10 shadow-[-5px_10px_5px] shadow-[#12121210]`}>
       <h1 className="font-semibold px-4 pt-4">Flood Alert </h1>
       <div className="text-white-100 mt-[10px]">
         <Swiper
@@ -72,9 +403,9 @@ export default function RightBar({ rightBarStatus }) {
           spaceBetween={30}
           freeMode={true}
           pagination={{ clickable: true }}
-          autoplay={{ delay: 2500, disableOnInteraction: false }}
+          autoplay={{ delay: 2500, disableOnInteraction: true }}
           modules={[FreeMode, Autoplay]}
-          className="mySwiper h-[calc(100vh-200px)]"
+          className="mySwiper h-[calc(120vh-200px)]"
         >
           {floodAlertData.map((floodData, i) => (
             <SwiperSlide key={i}>
